66be10d7a423f8755975dda005b628c0757ec626
Kyrylo <kirilltaranskyi@gmail.com>
2023-08-22 09:34:16 +0300

    sample added

---
+++ react_hw2/script.js
@@ -0,0 +1,19 @@
+var domContainer = document.querySelector('#root');
+var root = ReactDOM.createRoot(domContainer);
+
+var animals = [{ type: 'turtle', icon: '\uD83D\uDC22' }, { type: 'octopus', icon: '\uD83D\uDC19' }, { type: 'fish', icon: '\uD83D\uDC20' }, { type: 'flamingo', icon: '\uD83E\uDDA9' }, { type: 'penguin', icon: '\uD83D\uDC27' }];
+
+root.render(React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Animals Table'
+    ),
+    React.createElement(
+        'table',
+        null,
+        React.createElement('tbody', null)
+    )
+));
---
+++ react_hw2/src/script.js
@@ -0,0 +1,22 @@
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+
+const animals = [
+    {type: `turtle`, icon: `🐢`},
+    {type: `octopus`, icon: `🐙`},
+    {type: `fish`, icon: `🐠`},
+    {type: `flamingo`, icon: `🦩`},
+    {type: `penguin`, icon: `🐧`}
+]
+
+
+
+root.render(<React.Fragment>
+    <h1>Animals Table</h1>
+    <table>
+        <tbody>
+            
+        </tbody>
+    </table>
+
+</React.Fragment>)
